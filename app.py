ca93cda75244ad865e7e0ac7d9e4214ce8133ccf
blueag8 <ns.wickham08@outlook.com>
2020-01-21 16:35:02 +0000

    added base.html and about.html

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+
+from flask import Flask, render_template, redirect, request, url_for
+
+
+#connect to IMAGE_UPLOADS
+app = Flask(__name__)
+
+
+#set app variables
+
+
+
+
+
+#home page
+
+
+@app.route('/')
+def home():
+    return render_template("base.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+    
+    
+
+    
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(8000),
+            debug=True)
